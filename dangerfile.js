bf22e8338855f2396f5b7dd13c93f7119b9d1ec0
Evan Strat <evan10s@users.noreply.github.com>
2020-01-24 17:17:25 -0500

    Don't fail a PR for not updating the version if the only changes were to package.json or yarn.lock files (#45)

--- dangerfile.js
+++ dangerfile.js
@@ -1,11 +1,15 @@
 import {danger, fail, message, schedule, warn} from "danger";
 
+function onlyMetadataUpdates(fileMatch) {
+    return fileMatch.getKeyedPaths().edited.every((x) => x.includes("package.json") || x.includes("yarn.lock"));
+}
+
 if (!danger.github.pr.body.includes("#trivial") && danger.github.pr.body.length < 100) {
     fail("The pull request description must be longer.  Include #trivial to override this.")
 }
 
 const client = danger.git.fileMatch("client/**");
-if (client.edited) {
+if (client.edited && !onlyMetadataUpdates(client)) {
     schedule(danger.git.JSONDiffForFile("client/package.json").then(diff => {
         if (diff) {
             if (!diff.version) {
@@ -20,7 +24,7 @@ if (client.edited) {
 
 
 const server = danger.git.fileMatch("server/**");
-if (server.edited) {
+if (server.edited && !onlyMetadataUpdates(server)) {
     schedule(danger.git.JSONDiffForFile("server/package.json").then(diff => {
         if (diff) {
             if (!diff.version) {
@@ -34,7 +38,7 @@ if (server.edited) {
 }
 
 const root = danger.git.fileMatch("./*");
-if (root.edited) {
+if (root.edited && !onlyMetadataUpdates(root)) {
     schedule(danger.git.JSONDiffForFile("package.json").then(diff => {
         if (diff) {
             if (diff.dependencies || diff.devDependencies) {

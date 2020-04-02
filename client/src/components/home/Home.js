79f628d3edd3a8dbb614ed119c82f2181ca1a47b
seongryoo <seongryoo@gmail.com>
2020-04-02 13:48:40 -0400

    Add homepage CSS (#112)
    * Reorganize component css files in client src directory to clarify usage.
    
    CSS files relevant to a specific React component have been placed in the corresponding subdirectory. For example, styling for elements only on the homepage is now in src/components/home/style.css
    
    CSS files relevant to the entire website (e.g. container sizes, fonts, etc.) are in client/src/css, organized into SMACSS format.
    
    * Reorganize component css files in client src directory to clarify usage.
    
    CSS files relevant to a specific React component have been placed in the corresponding subdirectory. For example, styling for elements only on the homepage is now in src/components/home/style.css
    
    CSS files relevant to the entire website (e.g. container sizes, fonts, etc.) are in client/src/css, organized into SMACSS format.
    
    * Add css classes to Home.js to allow for foundational styling.
    
    * Add basic styling to homepage!
    
    * Update json
    
    * Add routing to map CTAs, clean up CSS
    
    Co-authored-by: Seong <sryoo6@gatech.edu>

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -6,39 +6,65 @@ import "./style.css";
 import MapCarrot from "./img/map_carrot.png";
 import MapConfused from "./img/map_confused.png";
 import Veggies from "./img/veggies_thinking.png";
+import {Link} from "react-router-dom";
 
 function Home(props) {
     return (
-        <Grid columns={"2"} verticalAlign={"middle"} textAlign={"center"} stackable>
+        <Grid columns={"2"} verticalAlign={"middle"} stackable>
+            {/*Hero banner*/}
             <Grid.Row className="hero">
-                <Grid.Column>
-                    <Header>Do I live in a food desert?</Header>
+
+                <Grid.Column className="info-left info">
+                    <Header>Do I live in an Atlanta food desert?</Header>
+                    <p className="hero-text">Scroll down to learn more about how <i className="emph"> where you live in Atlanta can affect the food you eat.</i></p>
+                    <Button className="main-cta cta" as={Link} to="/map">View our map</Button>
                 </Grid.Column>
-                <Grid.Column><Image className="hero-img" src={MapCarrot}/></Grid.Column>
+
+                <Grid.Column className="info-right info">
+                    <Image className="hero-img" src={MapCarrot}/>
+                </Grid.Column>
+
             </Grid.Row>
-            <Grid.Row columns={2}>
-                <Grid.Column><Image className="home-img" src={Veggies}/></Grid.Column>
-                <Grid.Column>
+
+
+            {/*Information sections*/}
+            <Grid.Row columns={2} className="info-row">
+
+                <Grid.Column className="info-left info">
+                    <Image className="home-img" src={Veggies}/>
+                </Grid.Column>
+
+                <Grid.Column className="info-right info">
                     <Header size={"large"} className="home-section-header">What is a food desert?</Header>
                     <p className="home-text">A food desert is an area where residents cannot easily access fresh,
                         healthy food.</p>
                 </Grid.Column>
+
             </Grid.Row>
 
-            <Grid.Row columns={2}>
-                <Grid.Column>
+            <Grid.Row columns={2} className="info-row">
+
+                <Grid.Column className="info-left info">
                     <Header size={"large"} className="home-section-header">Why do food deserts exist?</Header>
                     <p className="home-text">When stores that sell cheap, healthy food are too far from a community, it
                         can be difficult for residents to buy the food they need.</p>
                 </Grid.Column>
-                <Grid.Column><Image className="home-img" src={MapConfused}/></Grid.Column>
+
+                <Grid.Column className="info-right info">
+                    <Image className="home-img" src={MapConfused}/>
+                </Grid.Column>
+
             </Grid.Row>
-            <Grid.Row columns={1}>
-                <Grid.Column as={Header} size={"large"}>Am I living in a food desert?</Grid.Column>
+
+
+            {/*Call to action*/}
+            <Grid.Row columns={1} className="cta-row full-width-row">
+                <Grid.Column className="info-center info">
+                    <Header size="large">Am I living in a food desert?</Header>
+                    <Button className="main-cta cta" as={Link} to="/map" >Find out now</Button>
+                </Grid.Column>
             </Grid.Row>
-            <Grid.Row columns={1}>
-                <Grid.Column as={Header} size={"large"}><Button>Find out now</Button></Grid.Column>
-            </Grid.Row>
+
         </Grid>
     );
 }

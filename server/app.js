f62faeef47f4f5b5fdd7cbf3b63494adbf88107f
Evan Strat <evan10s@users.noreply.github.com>
2020-04-05 15:24:12 -0400

    Deployment fixes (#116)
    * Change template engine back to ejs to resolve dep issue
    
    * Add request-promise peer dependency
    
    * Removed unnecessary files and fixed up app.js to serve frontend
    
    * Style fix

--- server/app.js
+++ server/app.js
@@ -5,25 +5,26 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const mapRouter = require("./routes/map");
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "ejs");
 
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/map", mapRouter);
+app.use("/api/v1/map", mapRouter);
+
+app.get("*", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
